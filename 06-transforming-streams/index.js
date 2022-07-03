b34142609c7e84faf8a5134fa3dbdcda1ad3a72e
brunosm16 <brunoskr23@gmail.com>
2022-07-03 18:27:06 -0300

    transforming-streams.

---
+++ 06-transforming-streams/index.js
@@ -0,0 +1,19 @@
+import fs from "fs";
+import { Transform } from "stream";
+
+const rs = fs.createReadStream("./data/sample-text.txt");
+
+const sampleTextUpperCase = fs.createWriteStream(
+  "./data/sample-text-uppercase.txt"
+);
+
+const uppercase = new Transform({
+  transform(chunk, undefined, callback) {
+    callback(
+      null,
+      chunk.toString().toUpperCase()
+    );
+  },
+});
+
+rs.pipe(uppercase).pipe(sampleTextUpperCase);
